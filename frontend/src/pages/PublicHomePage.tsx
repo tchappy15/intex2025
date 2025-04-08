01d180c83de2087bfb48db9009bfb99af65a4d63
Tate Matley <tatematley@yourdomain.com>
2025-04-08 16:24:16 -0600

    css for login, register, and public home pages

--- frontend/src/pages/PublicHomePage.tsx
+++ frontend/src/pages/PublicHomePage.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom';
+import './PublicHomePage.css';
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to Our App</h1>
-      <p>This is a public landing page.</p>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    
+    <div className="home-page-container">
+      <div className="card">
+      <img src="/images/Logo.png" alt="Logo" className="login-logo" />
+        <h1>Discover A Personally Curated<br/>Collection of Niche Films<br/>Just For You!</h1>
+        <Link to="/login">
+          <button className="custom-btn">Sign in / Register</button>
+        </Link>
+      </div>
     </div>
   );
 }
