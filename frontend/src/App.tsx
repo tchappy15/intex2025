84ce1fa7043c08ce3e8c85527252b0127a7129f1
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-04-08 10:05:12 -0600

    added privacy skeleton and the footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import PublicHomePage from './pages/PublicHomePage';
 import AdminPage from './pages/AdminPage';
+import PrivacyPage from './pages/PrivacyPage';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -25,7 +27,9 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/admin" element={<AdminPage />} />
+            <Route path="/privacy" element={<PrivacyPage />} />
           </Routes>
+          <Footer />
         </Router>
       {/* </CartProvider> */}
     </>
