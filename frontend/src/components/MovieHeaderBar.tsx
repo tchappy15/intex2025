2722e7ea41639f247b8972761302500ca297d856
tchappy15 <153124713+tchappy15@users.noreply.github.com>
2025-04-09 21:00:07 -0600

    added Logout for proper log out functionality in movieheaderbar

--- frontend/src/components/MovieHeaderBar.tsx
+++ frontend/src/components/MovieHeaderBar.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './MovieHeaderBar.css';
 import { useNavigate } from 'react-router-dom';
 import { fetchGenres } from '../api/api';
+import Logout from './Logout';
 
 function MovieHeaderBar({
   selectedType,
@@ -109,7 +110,14 @@ function MovieHeaderBar({
           {dropdownOpen && (
             <div className="movie-dropdown-menu">
               <button onClick={() => navigate('/admin')}>Manage Movies</button> 
-              <button onClick={onLogout}>Logout</button>
+              
+              {/* added Logout for proper log out functionality */}
+              <Logout>
+                <button> 
+                  Logout
+              </button>
+              </Logout>
+              
             </div>
           )}
         </div>
